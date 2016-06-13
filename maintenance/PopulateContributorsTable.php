7fad12a507abdf65683b3236f0e95d48f0cf444b
Devi R <devikrishnan67@gmail.com>
2016-06-13 16:29:58 +0530

    Create a maintenance script to populate the Contributors Table
    The User Id, Usertext, Page Id are taken from the revision
    table and then grouped to get the revision count each user
    has made to a page and all the information is then populated
    to the contributors table .
    
    Bug: T134938
    Change-Id: Ib45acd2400ee5116c0a68ac7fe6556442097fe3f

---
+++ maintenance/PopulateContributorsTable.php
@@ -0,0 +1,82 @@
+<?php
+
+if (getenv('MW_INSTALL_PATH') !== false) {
+	require_once(getenv('MW_INSTALL_PATH') . '/maintenance/Maintenance.php');
+} else {
+	require_once(__DIR__ . '/../../../maintenance/Maintenance.php');
+}
+
+/**
+ * Maintenance script that populates the Contributors table with Contributor's data
+ *
+ * @ingroup Maintenance
+ */
+class PopulateContributorsTable extends Maintenance {
+
+	public function __construct() {
+		parent::__construct();
+		$this->addDescription( "Populates the contributor's table with contributor's data" );
+		$this->setBatchSize( 100 );
+	}
+
+	public function execute() {
+		$this->output( "Started processing..\n" );
+		$dbw = $this->getDB( DB_MASTER );
+		$dbr = $this->getDB( DB_SLAVE );
+
+		$start = $dbr->selectField( 'revision', 'MIN(rev_page)', false, __METHOD__ );
+		if ( !$start ) {
+			$this->output( "Nothing to do.\n" );
+
+			return true;
+		}
+		$end = $dbr->selectField( 'revision', 'MAX(rev_page)', false, __METHOD__ );
+
+		$end += $this->mBatchSize - 1;
+		$blockStart = $start;
+		$blockEnd = $start + $this->mBatchSize - 1;
+		while ( $blockEnd <= $end ) {
+			$this->output( "Getting Contributor's data..\n" );
+			$cond =array( "rev_page BETWEEN $blockStart AND $blockEnd" );
+			$res = $dbr->select(
+				'revision',
+				array( 'COUNT(*) AS cn_revision_count', 'rev_user', 'rev_user_text', 'rev_page' ),
+				$cond,
+				__METHOD__,
+				array( 'GROUP BY' => array( 'rev_page', 'rev_user','rev_user_text' ) )
+			);
+
+			$this->output( "Writing data into Contributors Table.. \n" );
+
+			foreach ( $res as $row ) {
+				$dbw->upsert(
+					'contributors',
+					array(
+						'cn_page_id' => $row->rev_page,
+						'cn_user_id' => $row->rev_user,
+						'cn_user_text' => $row->rev_user_text,
+						'cn_revision_count' => $row->cn_revision_count
+					),
+					array(
+						'cn_page_id',
+						'cn_user_id',
+						'cn_user_text'
+					),
+					array(
+						'cn_page_id' => $row->rev_page,
+						'cn_user_id' => $row->rev_user,
+						'cn_user_text' => $row->rev_user_text,
+						'cn_revision_count' => $row->cn_revision_count
+					),
+					__METHOD__
+				);
+			}
+			$blockStart += $this->mBatchSize;
+			$blockEnd += $this->mBatchSize;
+			$this->output( "Process finished.\n" );
+		}
+	}
+}
+
+$maintClass = "PopulateContributorsTable";
+require_once RUN_MAINTENANCE_IF_MAIN;
