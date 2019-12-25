ec5ddc0192520a56b977a448a539db651ba1e1e3
Zoranzoki21 <zorandori4444@gmail.com>
2019-12-25 09:03:01 +0100

    Add magic words for Serbian language
    Change-Id: If1e5529f49872563cf1122a040cbf488dd513951

--- Contributors.magic.php
+++ Contributors.magic.php
@@ -12,3 +12,17 @@ $magicWords = [];
 $magicWords['en'] = [
 	'contributors' => [ 0, 'contributors' ],
 ];
+
+/** Serbian Cyrillic (српски (ћирилица))
+ * @author Zoranzoki21
+ */
+$magicWords['sr-ec'] = [
+	'contributors' => [ 0, 'доприносиоци' ],
+];
+
+/** Serbian Latin (srpski (latinica))
+ * @author Zoranzoki21
+ */
+$magicWords['sr-el'] = [
+	'contributors' => [ 0, 'doprinosioci' ],
+];
