3bfdc84ebe6610279efda667751697646aa5cd4c
Umherirrender <umherirrender_de.wp@web.de>
2021-12-06 16:31:44 +0100

    Emit deprecation warning from php entry point
    Change-Id: Ia6368480955d68d2d45b04a0513049422802c33e

--- Contributors.php
+++ Contributors.php
@@ -5,12 +5,12 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	$wgMessagesDirs['Contributors'] = __DIR__ . '/i18n';
 	$wgExtensionMessagesFiles['ContributorsAlias'] = __DIR__ . '/Contributors.alias.php';
 	$wgExtensionMessagesFiles['ContributorsMagic'] = __DIR__ . '/Contributors.magic.php';
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for Contributors extension. ' .
 		'Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return;
 } else {
-	die( 'This version of the Contributors extension requires MediaWiki 1.25+' );
+	die( 'This version of the Contributors extension requires MediaWiki 1.35+' );
 }
